e139c1f8b5328218ab8f1bbdc55f4384f63780e0
Tomas Janecka <xjaneck1@fi.muni.cz>
2021-08-10 15:26:30 +0200

    init

---
+++ recovery-api/routes/api.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const express = require("express");
+let router: Router = express.Router();
+
+// router.use("/property", property);
+// router.use("/user", user);
+
+export default router;
---
+++ recovery-api/src/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+const port: number = 5000;
+const app = express();
+
+async function main() {
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
